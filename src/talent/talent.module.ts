cf9fa5f601631080ba1242188a340e9b602789ce
DigamaSyx <auga1sports@gmail.com>
2024-12-02 11:47:48 -0300

    fix(): added missing import for `Char` entity

--- src/talent/talent.module.ts
+++ src/talent/talent.module.ts
@@ -3,9 +3,10 @@ import { TalentController } from './talent.controller';
 import { TalentService } from './talent.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Talent } from './entity/talent.entity';
+import { Char } from 'src/char/entity/char.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Talent])],
+  imports: [TypeOrmModule.forFeature([Talent, Char])],
   controllers: [TalentController],
   providers: [TalentService],
 })
