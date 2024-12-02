380a4ced7493818dc5c5c30c5f383ed0bdf2c303
DigamaSyx <auga1sports@gmail.com>
2024-12-02 11:51:27 -0300

    chore(): expand present type

--- src/talent/types/talent.types.ts
+++ src/talent/types/talent.types.ts
@@ -1,1 +1,21 @@
-export type Stats = 'atk' | 'hp' | 'def' | 'spd';
+export type Stats =
+  | 'atk'
+  | 'hp'
+  | 'def'
+  | 'spd'
+  | 'break_effect'
+  | 'crit_dmg'
+  | 'crit_rate'
+  | 'effect_hit_rate'
+  | 'effect_res'
+  | 'energy_regen'
+  | TypeBoost;
+type TypeBoost = `${Types}_dmg_bonus`;
+type Types =
+  | 'physical'
+  | 'fire'
+  | 'ice'
+  | 'wind'
+  | 'lightning'
+  | 'quantum'
+  | 'imaginary';
