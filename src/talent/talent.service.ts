b4c809c0bf3a7561056dd4d41c10981984b0830d
DigamaSyx <auga1sports@gmail.com>
2024-12-16 06:45:33 -0300

    feat(): added 2 new routes in the `Talent.service`

--- src/talent/talent.service.ts
+++ src/talent/talent.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Talent } from './entity/talent.entity';
 import { Repository } from 'typeorm';
@@ -20,6 +20,30 @@ export class TalentService {
     await this.talentRepo.save(talent);
     return {
       message: `Talent for 'char': ${char.name}, was created.`,
+    };
+  }
+  // ! Testar
+  async find(id_or_char: number | string) {
+    let talent: Talent;
+    if (typeof id_or_char === 'number') {
+      talent = await this.talentRepo.findOneBy({ talent_id: id_or_char });
+    } else {
+      talent = await this.talentRepo.findOneBy({ char: { name: id_or_char } });
+    }
+    if (!talent)
+      throw new BadRequestException(
+        `Talent with ${typeof id_or_char === 'number' ? 'specified Id:' : 'associated Char:'} ${id_or_char} was not found.`,
+      );
+    return talent;
+  }
+  // ! Testar
+  async remove(id: number) {
+    const talent = await this.talentRepo.findOneBy({ talent_id: id });
+    if (!talent)
+      throw new BadRequestException(`Talent with Id: ${id}, does not exists.`);
+    await this.talentRepo.remove(talent);
+    return {
+      message: `The talent associated with the Char: ${talent.char.name} was removed.`,
     };
   }
 }
