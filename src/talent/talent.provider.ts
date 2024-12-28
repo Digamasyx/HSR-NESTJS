2c17e219d1a90387a154bad2671b6be740692b14
DigamaSyx <auga1sports@gmail.com>
2024-12-28 09:48:09 -0300

    feat(): add TalentProvider for validating TalentDTO
    Introduce a new TalentProvider to handle validation for TalentDTO.
    Include type guard function to check for valid stat types and ensure the
    value is within acceptable limits.
    
    This provider encapsulates the logic for validating talent data,
    improving code modularity and maintainability. By centralizing
    validation logic, we ensure consistent validation across different parts
    of the application.

---
+++ src/talent/talent.provider.ts
@@ -0,0 +1,43 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Stats } from './types/talent.types';
+import { TalentDTO } from './dto/talent.dto';
+
+@Injectable()
+export class TalentProvider {
+  checkBody(arg: TalentDTO) {
+    function isStats(arg: any): arg is Stats {
+      const possibleStats = [
+        'atk',
+        'hp',
+        'def',
+        'spd',
+        'break_effect',
+        'crit_dmg',
+        'crit_rate',
+        'effect_hit_rate',
+        'effect_res',
+        'energy_regen',
+        'physical_dmg_bonus',
+        'fire_dmg_bonus',
+        'ice_dmg_bonus',
+        'wind_dmg_bonus',
+        'lightning_dmg_bonus',
+        'quantum_dmg_bonus',
+        'imaginary_dmg_bonus',
+      ];
+      if (possibleStats.includes(arg)) return true;
+      throw new BadRequestException(
+        `Wrong Stat type inserted.\nWas expected: '${possibleStats.join(', ')}'.\nWas Recieved: ${arg}.`,
+      );
+    }
+    function isValidValue(arg: number) {
+      if (arg < 0 || arg > 50)
+        throw new BadRequestException(
+          `Value inserted can't be ${arg < 0 ? 'negative' : 'too big'}.`,
+        );
+      return true;
+    }
+
+    return isStats(arg.stat) && isValidValue(arg.value);
+  }
+}
