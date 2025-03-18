0e954ad18204e00c461e55e1a0e44793cf9cedbd
DigamaSyx <auga1sports@gmail.com>
2025-03-18 19:08:05 -0300

    feat: add ValidateStringEnumPipe for enhanced string and enum validation
    - Implements a custom validation pipe (ValidateStringEnumPipe) using
      NestJS.
    - Ensures input is alphabetic or matches predefined enums (Paths,
      Types).
    - Throws BadRequestException with a clear message on validation
      failure.

---
+++ src/char/pipes/string-enum.pipe.ts
@@ -0,0 +1,21 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { Paths, Types } from '../enums/char.enum';
+
+@Injectable()
+export class ValidateStringEnumPipe implements PipeTransform {
+  transform(value: string) {
+    const regex = /^[a-zA-Z]+$/;
+    const validEnums = [
+      ...(Object.values(Paths) as string[]),
+      ...(Object.values(Types) as string[]),
+    ];
+
+    if (regex.test(value) || validEnums.includes(value)) {
+      return value;
+    }
+
+    throw new BadRequestException(
+      `Validation failed. The value '${value}' must be alphabetic or match one of the enums: [${validEnums.join(', ')}]`,
+    );
+  }
+}
