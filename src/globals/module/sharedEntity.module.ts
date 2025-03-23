7aa2997d9ac40b60598bcadb374736c3678e6d7d
DigamaSyx <auga1sports@gmail.com>
2025-03-23 20:44:41 -0300

    feat: incorporate Files entity into shared module architecture
    - Imported `Files` entity to the shared modules.
    - Added `FilesSharedModule`:
       - Configured `TypeOrmModule` for `Files` entity.
       - Exposed `FilesSharedModule` for usage across the application.
    - Updated imports to maintain organization and ensure the
      newly added `Files` entity integrates seamlessly with other modules.

--- src/globals/module/sharedEntity.module.ts
+++ src/globals/module/sharedEntity.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Char } from 'src/char/entity/char.entity';
+import { Files } from 'src/file/entity/file.entity';
 import { Talent } from 'src/talent/entity/talent.entity';
 import { User } from 'src/user/entities/user.entity';
 
@@ -21,3 +22,9 @@ export class TalentSharedModule {}
   exports: [TypeOrmModule],
 })
 export class CharSharedModule {}
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Files])],
+  exports: [TypeOrmModule],
+})
+export class FilesSharedModule {}
