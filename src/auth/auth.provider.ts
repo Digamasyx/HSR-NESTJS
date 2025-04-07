8ef00f4384ad0ee0e92548feb269443b6585d048
DigamaSyx <auga1sports@gmail.com>
2025-04-07 10:16:39 -0300

    refactor(providers): migrate auth-specific utilities to global provider
    auth/auth.provider.ts:
      - Removed `formatDate` method from `AuthProvider` to improve modularity.
      - Retained authentication-specific logic (`checkIfExistsAndCompare`).
    
    globals/providers/global.provider.ts:
      - Introduced `GlobalProvider` to handle non-authentication-specific utilities.
      - Moved `formatDate` function to `GlobalProvider` for broader application usage.

--- src/auth/auth.provider.ts
+++ src/auth/auth.provider.ts
@@ -7,17 +7,6 @@ import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthProvider {
-  formatDate(arg: Date) {
-    const day = String(arg.getDate()).padStart(2, '0');
-    //? getMonth() retorna 0-11, então adiciona-se 1
-    const month = String(arg.getMonth() + 1).padStart(2, '0');
-    const hours = String(arg.getHours()).padStart(2, '0');
-    const minutes = String(arg.getMinutes()).padStart(2, '0');
-    const seconds = String(arg.getSeconds()).padStart(2, '0');
-    const year = String(arg.getFullYear());
-
-    return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds}`;
-  }
   async checkIfExistsAndCompare(arg: any, pass: string) {
     if (!arg)
       throw new NotFoundException(
---
+++ src/globals/provider/global.provider.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class GlobalProvider {
+  formatDate(arg: Date) {
+    const day = String(arg.getDate()).padStart(2, '0');
+    //? getMonth() retorna 0-11, então adiciona-se 1
+    const month = String(arg.getMonth() + 1).padStart(2, '0');
+    const hours = String(arg.getHours()).padStart(2, '0');
+    const minutes = String(arg.getMinutes()).padStart(2, '0');
+    const seconds = String(arg.getSeconds()).padStart(2, '0');
+    const year = String(arg.getFullYear());
+
+    return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds}`;
+  }
+}
