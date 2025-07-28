5e1ce5ade7cf3f439658e296e9aa1b8cebaca414
DigamaSyx <88753741+Digamasyx@users.noreply.github.com>
2025-07-27 21:18:13 -0300

    refactor(): migrate TypeORM config to async using ConfigService

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.HOST,
-      port: Number(process.env.PORT),
-      username: process.env.USERNAME,
-      password: process.env.PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/../**/*.entity.{ts,js}'],
-      synchronize: true, //! Não usar em prod
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('DB_HOST'),
+        port: Number(config.get('DB_PORT')),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity.{ts,js}'],
+        synchronize: true, //! Não usar em prod
+      }),
+      inject: [ConfigService],
     }),
   ],
 })
